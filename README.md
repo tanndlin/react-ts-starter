15620ac22c0a8801341b227163e20a8e2003cde2
Tanndlin <tanndlin@gmail.com>
2023-04-22 16:44:56 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Note Taker
+
+Note Taker is a web app created in React for taking interactive notes in
+Markdown.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="py-4 h-screen w-screen bg-primary">Hello World!</div>
+    );
+}
+
+export default App;
